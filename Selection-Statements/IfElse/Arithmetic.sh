3cc3afb3b53175dffa54dc6c0eaf95be47a420d5
sweetylenka <sweetylenka2013@gmail.com>
2021-02-25 10:09:33 +0530

    Enter 3 Numbers do following arithmetic operation and find the one that
    is maximum and minimum
    1. a + b * c 3. c + a / b
    2. a % b + c 4. a * b + c

---
+++ Selection-Statements/IfElse/Arithmetic.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -x
+
+echo "Enter first number :"
+read num1
+
+echo "Enter second number :"
+read num2
+
+
+echo "Enter third number :"
+read num3
+
+Operation1=$(($num1+$num2*$num3))
+echo $Operation1
+
+
+Operation2=$(($num1%$num2+$num3))
+echo $Operation2
+
+Operation3=$(($num3+$num1/$num2))
+echo $Operation3
+
+Operation4=$(($num1*$num2+$num3))
+echo $Operation4
+
+
+if [ $num1 -gt $num2 ] && [ $num1 -gt $num3 ]
+then
+		echo "$num1 is maximum"
+elif [ $num2 -gt  $num1 ] && [$num2 -gt $num3 ]
+then
+		echo "$num2 is maximum"
+else
+		echo "$num3 is maximum"
+fi
+
+
+if [ $num1 -lt $num2 ] && [ $num1 -lt $num3 ]
+then
+      echo "$num1 is minimum"
+elif [ $num2 -lt  $num1 ] && [$num2 -lt $num3 ]
+then
+      echo "$num2 is minimum"
+else
+      echo "$num3 is minimum"
+fi
+
+
+
+
+
+
+ 
