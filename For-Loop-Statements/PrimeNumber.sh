2dbe35aba5fafcbdb0ddf439622c6c6c5b05a7b5
sweetylenka <sweetylenka2013@gmail.com>
2021-03-01 10:32:37 +0530

    Write a program that takes a input and determines if the number is a prime.

---
+++ For-Loop-Statements/PrimeNumber.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+echo "Enter a number :"
+read num
+
+for((i=2;i<=num/2;i++))
+do	
+		if [ $((num%i)) -eq 0 ]
+		then
+			echo "$num is not a prime number"
+		else
+			echo "$num is a prime number"
+		fi
+done
