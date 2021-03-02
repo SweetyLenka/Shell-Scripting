69f4cb8da34ac6eefb1f8fdaa401b8414a90d913
sweetylenka <sweetylenka2013@gmail.com>
2021-03-02 16:41:11 +0530

    Extend the Flip Coin problem till either Heads or Tails wins 11 times.

---
+++ FlipCoin.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+
+FlipCoin=$((RANDOM%2))
+Head=0
+Tail=0
+
+while [[ $Head -lt 11  &&  $Tail -lt 11 ]]
+do 
+		 if [ $FlipCoin -eq 1 ]
+		 then
+				 Head=$(($Head+1))
+		 else
+				 Tail=$(($Tail+1))
+		 fi
+done
+if [ $Head -eq 11 ]
+then
+		echo "Head won 11 times"
+else
+		echo "Tail won 11 times"
+fi
