55912b6aa29a08c75ae072d51d6bcb51c6a6ae4a
SweetyLenka <55683708+SweetyLenka@users.noreply.github.com>
2021-03-04 09:51:15 +0530

    Update LeapYear.sh

--- Selection-Statements/LeapYear.sh
+++ Selection-Statements/LeapYear.sh
@@ -2,15 +2,21 @@
 
 read -p "Enter a Year " year
 
-if (( $year % 400 == 0 ))
-then
-		echo $year "is  a leap year"
-elif (( $year % 100 == 0 ))
-then
-		echo $year "is not a leap year"
-elif (( $year % 4 == 0 ))
-then
+if [ $year -ge 1000 ] && [ $year -le 9999 ]
+   then
+   if (( $year % 400 == 0 ))
+   then
+      echo $year "is  a leap year"
+   elif (( $year % 100 == 0 ))
+   then
+      echo $year "is not a leap year"
+   elif (( $year % 4 == 0 ))
+   then
       echo $year "is a leap year"
-else
-		echo $year "is not a leap year"
+   else
+      echo $year "is not a leap year"
+   fi
+   else
+      echo "Enter  a valid year"
 fi
+
